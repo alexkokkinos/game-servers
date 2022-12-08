b6acfcb32d4ed0b023f585a5794d50591f4359a1
alexkokkinos <kokkinos.alex@gmail.com>
2022-12-07 23:14:37 -0600

    Fixed everything, guaranteed

--- production.tf
+++ production.tf
@@ -2,19 +2,18 @@ module "minecraft" {
   source         = "./modules/hetzner-game-server"
   server_type    = "CPX31"
   game_firewalls = [hcloud_firewall.minecraft, hcloud_firewall.ssh]
-  ssh_keys       = [hcloud_ssh_key.games.public_key]
+  ssh_keys       = [data.hcloud_ssh_key.games.public_key]
 }
 
-resource "hcloud_ssh_key" "games" {
-  name       = "Game Server SSH Keys"
-  public_key = file("~/.ssh/hetzner.pub")
+data "hcloud_ssh_key" "games" {
+  name = "Hetzner"
 }
 
 resource "hcloud_firewall" "minecraft" {
   name = "minecraft-firewall-tcp"
   rule {
     direction = "in"
-    protocol  = "TCP"
+    protocol  = "tcp"
     port      = "25565"
     source_ips = [
       "0.0.0.0/0",
@@ -23,7 +22,7 @@ resource "hcloud_firewall" "minecraft" {
   }
   rule {
     direction = "in"
-    protocol  = "UDP"
+    protocol  = "udpx"
     port      = "25565"
     source_ips = [
       "0.0.0.0/0",
@@ -36,7 +35,7 @@ resource "hcloud_firewall" "ssh" {
   name = "game-server-ssh"
   rule {
     direction = "in"
-    protocol  = "TCP"
+    protocol  = "tcp"
     port      = "22"
     source_ips = [
       "0.0.0.0/0",
