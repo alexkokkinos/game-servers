247d38e2e6787b6cdfc5aad79af98d2e05339ccf
alexkokkinos <kokkinos.alex@gmail.com>
2022-12-09 22:50:33 -0600

    Allow the world in

--- production.tf
+++ production.tf
@@ -30,6 +30,16 @@ resource "hcloud_firewall" "minecraft" {
       "::/0"
     ]
   }
+
+  rule {
+    direction = "in"
+    protocol = "tcp"
+    port = "8443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0" 
+    ]
+  }
 }
 
 resource "hcloud_firewall" "ssh" {
