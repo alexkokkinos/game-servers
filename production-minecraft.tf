3dc3173f3f146a9c8144c40b42285b400844e2eb
Alex Kokkinos <alex@alexkokkinos.com>
2023-10-18 18:50:11 -0500

    Created Christmas 2024 Minecraft Server

--- production-minecraft.tf
+++ production-minecraft.tf
@@ -1,31 +1,31 @@
 # Deleted
 
-# module "minecraft" {
-#   source            = "./modules/hetzner-game-server"
-#   server_name       = "minecraft"
-#   server_type       = "cpx31"
-#   game_firewall_ids = [hcloud_firewall.minecraft.id, hcloud_firewall.ssh.id]
-#   ssh_keys          = [data.hcloud_ssh_key.games.id]
-# }
+module "minecraft" {
+  source            = "./modules/hetzner-game-server"
+  server_name       = "minecraft"
+  server_type       = "cpx31"
+  game_firewall_ids = [hcloud_firewall.minecraft.id, hcloud_firewall.ssh.id]
+  ssh_keys          = [data.hcloud_ssh_key.games.id]
+}
 
-# resource "hcloud_firewall" "minecraft" {
-#   name = "minecraft-firewall-tcp"
-#   rule {
-#     direction = "in"
-#     protocol  = "tcp"
-#     port      = "25565"
-#     source_ips = [
-#       "0.0.0.0/0",
-#       "::/0"
-#     ]
-#   }
-#   rule {
-#     direction = "in"
-#     protocol  = "udp"
-#     port      = "25565"
-#     source_ips = [
-#       "0.0.0.0/0",
-#       "::/0"
-#     ]
-#   }
-# }
+resource "hcloud_firewall" "minecraft" {
+  name = "minecraft-firewall-tcp"
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "25565"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+  rule {
+    direction = "in"
+    protocol  = "udp"
+    port      = "25565"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
